f068d3aa93636fd7d89c9e757fdc97370f9295be
vovkahmil <vovkahmil97@gmail.com>
2024-01-29 15:48:38 +0200

    replace type on fill

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -2,15 +2,15 @@
 const { test, expect } = require('@playwright/test');
 
 
-test('login as admin', async({page}) => {
+test('login as admin', async ({ page }) => {
   const url = 'https://dev-app.creativelysquared.com/';
   const emailLocator = page.locator("xpath=//input[@name='email']");
   const passwordLocator = page.locator("xpath=//input[@name='password']");
   const loginBtn = page.locator("xpath=//button[@type='submit' and contains(normalize-space(), 'Login')]");
-  
+
 
   await page.goto(url);
-  await emailLocator.type('volo');
-  await passwordLocator.type('khmil');
-  await loginBtn.click(); 
+  await emailLocator.fill('volo');
+  await passwordLocator.fill('khmil');
+  await loginBtn.click();
 })
